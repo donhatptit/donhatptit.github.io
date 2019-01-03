106a68cfee77cd6c73087171c46d7c3576a9f912
Đỗ Nhất <dvnit04@gmail.com>
2019-01-03 14:43:19 +0700

    update

--- js/layout.js
+++ js/layout.js
@@ -43,3 +43,18 @@ if (window.outerWidth <= 768) {
 //         }
 //     })
 // }
+// Go to Top
+var toTop = document.getElementById("goto-top");
+  
+  toTop.addEventListener("click", function(){
+  scrollToTop(4000);
+});
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
+}
